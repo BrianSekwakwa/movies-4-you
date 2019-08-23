d27693633b28c06673e906363d756a3e4c1b8b9c
Brian Sekwakwa <briansenpai14@gmail.com>
2019-08-23 18:48:59 +0200

    added the handle function for the search input

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,30 +1,43 @@
-import React from "react";
+import React, {Component} from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import FormControl from "react-bootstrap/FormControl";
 import Nav from "react-bootstrap/Nav";
 
-function Navigation() {
-  return (
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-      <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-      <Navbar.Collapse id="responsive-navbar-nav">
-        <Nav className="mr-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#features">Features</Nav.Link>
-          <Nav.Link href="#pricing">Pricing</Nav.Link>
-        </Nav>
-        <Form inline>
-          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-          <Button className="botton-margin" variant="outline-info">
-            Search
-          </Button>
-        </Form>
-      </Navbar.Collapse>
-    </Navbar>
-  );
+class Navigation extends Component {
+  state = {
+    movieName: null
+  }
+
+  handleChange = (e) => {
+    console.log(e.target.value)
+  }
+
+  handleSubmit = (e) => {
+    console.log(e.target.value);
+  }
+  render() {
+    return (
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+          <Form inline>
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+            <Button className="botton-margin" variant="outline-info">
+              Search
+            </Button>
+          </Form>
+        </Navbar.Collapse>
+      </Navbar>
+    );
+  }
 }
 
 export default Navigation;
