2d45e61e31349078abc44289e4de8b32a3fd1544
Brian Sekwakwa <briansenpai14@gmail.com>
2019-08-25 17:33:36 +0200

    added the markup for the home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,11 @@ import React from "react";
 
 function Home() {
   return (
-    <div>
-      <div>Some Home component is going to go here</div>
+    <div className="home">
+      <h1>Movies 4 You</h1>
+      <p>
+        Check out all of the latest and upcoming movies playing at your theaters
+      </p>
     </div>
   );
 }
