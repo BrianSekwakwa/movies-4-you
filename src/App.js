a4defb40c303b5fdd1f5fa805f72a425d2985c31
Brian Sekwakwa <briansenpai14@gmail.com>
2019-08-29 18:05:48 +0200

    added the config file for the scss

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from "react";
 import Navigation from "./components/Navigation";
+import Home from "./components/Home";
+import Main from "./components/Main";
+import SearchMovie from "./components/SearchMovie";
 
 function App() {
   return (
