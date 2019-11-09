f063c60a50b535d7ebcf46d8b733c06ebe4b2d87
Brian Sekwakwa <briansenpai14@gmail.com>
2019-11-09 09:50:46 +0200

    Updates

---
+++ precache-manifest.f3a6d9698c6bfdc2e78eb09c5de740f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66ac5c93f510f4884a13d71b8ec0d059",
+    "url": "/movies-4-you/index.html"
+  },
+  {
+    "revision": "8ca572666ca1c1217e64",
+    "url": "/movies-4-you/static/css/main.66cc4ca3.chunk.css"
+  },
+  {
+    "revision": "75d45290cbce2a5ff2c5",
+    "url": "/movies-4-you/static/js/2.cc4f2b45.chunk.js"
+  },
+  {
+    "revision": "8ca572666ca1c1217e64",
+    "url": "/movies-4-you/static/js/main.c0d8b316.chunk.js"
+  },
+  {
+    "revision": "8ed964019cbbc065bf10",
+    "url": "/movies-4-you/static/js/runtime~main.3ee68ee5.js"
+  },
+  {
+    "revision": "de8d6e95e8749293315fd0ad816d0116",
+    "url": "/movies-4-you/static/media/home.de8d6e95.jpg"
+  }
+]);
